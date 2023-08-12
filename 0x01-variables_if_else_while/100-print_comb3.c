4a5ac386ed5910985ce41bbc39141aaaf6c5af9f
HamzaMarjane <hamza.marjanee@gmail.com>
2023-08-11 17:23:36 -0700

    Variables_if_else_while

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -4,7 +4,7 @@
  * main - Prints all possible combinations of two different digits,
  * in ascending order, separated by a comma followed by a space.
  *
- * Return: Always 0.
+ * Return: Always 0 (Success).
  */
 int main(void)
 {
