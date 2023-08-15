cbc5180347513aab344b2d2da36f68310a170b8a
HamzaMarjane <hamza.marjanee@gmail.com>
2023-08-15 16:34:50 -0700

    0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - alphabets printing.
+ *
+ * Return: 0 Always
+ *
+ *
+ *
+ *
+ */
+
+void print_alphabet(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+		_putchar(alpha);
+
+	_putchar('\n');
+}
