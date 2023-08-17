56b426de8379f34b0b47b36104bb511eb52b2198
HamzaMarjane <hamza.marjanee@gmail.com>
2023-08-17 16:14:11 -0700

    printdiagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,29 +1,34 @@
 #include "main.h"
 
 /**
- * print_diagonal - printing a diagonal in the terminal.
+ * print_diagonal - entry point
  *
- * Return: 0 Always.
- *
- *
+ * Description: Prints diagonals
+ *@n: number of spaces
+ * Return: void
  */
 
 void print_diagonal(int n)
 {
-int i;
-int j;
-if (n <= 0)
-{
-_putchar('\n');
-return;
-}
-for (i = 0; i < n; i++)
-{
-for (j = 0; j < i; j++)
-{
-_putchar(' ');
-}
-_putchar('\\');
-_putchar('\n');
-}
+	int row, num_lines;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (row = 1; row <= n; row++)
+		{
+			if (row > 1)
+			{
+				for (num_lines = 1; num_lines <= row - 1; num_lines++)
+				{
+				_putchar(' ');
+				}
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
 }
