9ac705512d6ecc26355d64cfe955c9990438f7df
HamzaMarjane <hamza.marjanee@gmail.com>
2023-08-21 16:55:02 -0700

    0x02-functions_nested_loops

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - update var value.
+ *
+ *
+ * the pointer points to to 98.
+ *
+ *
+ * @n: pointer to update var.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
