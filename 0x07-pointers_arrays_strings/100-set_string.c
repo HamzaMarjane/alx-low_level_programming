1ac558a890789d0b013c6ac3810b939c9befa951
HamzaMarjane <hamza.marjanee@gmail.com>
2023-08-28 19:02:48 -0700

    0x02-functions_nested_loops

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * set_string - sets the value of a pointer to a char
+ * @s: source ad.
+ * @to: target ad.
+ * Return: nothing to return (void).
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
