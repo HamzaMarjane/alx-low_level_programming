decc476fc18b9500c324355ef7cb769ba987c81f
HamzaMarjane <hamza.marjanee@gmail.com>
2023-08-30 16:38:07 -0700

    0x02-functions_nested_loops

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+int is_prime(int n, int i);
+/**
+ * is_prime_number - checks if n is prime
+ * @n: the number
+ * Return: 1 or 0 depending n if its prime or not.
+ */
+int is_prime_number(int n)
+{
+if (n <= 1)
+return (0);
+return (is_prime(n, n - 1));
+}
+/**
+ * is_prime - defines if the number is prime
+ * @n: the number to be checked
+ *
+ * @k: other numbers
+ * Return: 1 if prime number otherwise return 0.
+ */
+int is_prime(int n, int k)
+{
+if (k == 1)
+return (1);
+if (n % k == 0 && k > 0)
+return (0);
+return (is_prime(n, k - 1));
+}
