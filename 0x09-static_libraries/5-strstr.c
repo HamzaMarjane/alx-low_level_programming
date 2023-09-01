b755cf03bfb9b2b399aa3a27aeff3d5afd8cb852
HamzaMarjane <hamza.marjanee@gmail.com>
2023-09-01 16:53:09 -0700

    0x02-functions_nested_loops

---
+++ 0x09-static_libraries/5-strstr.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * _strstr - first occurrence of the substring.
+ * @haystack: main str to be examined
+ * @needle: searched in haystack
+ * Return: return 0
+ *
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int index;
+
+	if (*needle == 0)
+		return (haystack);
+
+	while (*haystack)
+	{
+		index = 0;
+
+		if (haystack[index] == needle[index])
+		{
+			do {
+				if (needle[index + 1] == '\0')
+					return (haystack);
+
+				index++;
+
+			} while (haystack[index] == needle[index]);
+		}
+
+		haystack++;
+	}
+
+	return ('\0');
+}
