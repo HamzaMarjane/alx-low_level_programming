bf5700a30c9850fa820d138e308dd4611aa8d549
HamzaMarjane <hamza.marjanee@gmail.com>
2023-10-19 12:38:04 -0700

    HARDTHINGS

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+/**
+ * add_node - adds a node at the head of the list.
+ * @head: head of the list.
+ * @str: string to add in the node.
+ *
+ * Return: the address of the head.
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+list_t *new;
+unsigned int len = 0;
+while (str[len])
+len++;
+new = malloc(sizeof(list_t));
+if (!new)
+return (NULL);
+new->str = strdup(str);
+new->len = len;
+new->next = (*head);
+(*head) = new;
+return (*head);
+}
